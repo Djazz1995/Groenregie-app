2f41ce8c4edf62b288c904433ada3337bf6b4b87
Djazz van der Hoogt <djazz.vanderhoogt@drukwerkdeal.nl>
2019-01-24 17:33:35 +0100

    removed customer reaction specific and added bold project titles

--- src/AppBundle/Controller/CustomerReactionsController.php
+++ src/AppBundle/Controller/CustomerReactionsController.php
@@ -29,30 +29,4 @@ class CustomerReactionsController extends Controller
             'subActive' => 'none'
         ]);
     }
-
-    /**
-     * @Route("/klantreacties/{name}/{id}", name="customer_reaction_specific")
-     */
-    public function partnerAction(Request $request, $name, $id)
-    {
-        $client = new \Contentful\Delivery\Client('4113452de26e27759b744718344945d5afc601e64e217688758dbfd649076208', 'jjzsv0091dq4', 'master');
-
-        $entry = $client->getEntry($id); // Dit id komt uit Contentful -> Content -> klantreactie-> entry ID
-
-        // replace this example code with whatever you need
-        return $this->render('default/customerReactions/customer_reaction_specific.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-            'pageTitle' => $entry->getNaam(),
-            'pageHeader' => $entry->getNaam(),
-            'image' => $entry->getThumbnail(),
-            'content' => $entry->getVolleText(),
-            'contentAuthor' => $entry->getAuteur(),
-            'parentActive' => 'customerReactions',
-            'subActive' => 'none',
-            'project' => $entry->getProject(),
-            'imageWidth' => $entry->getAfbeeldingBreedte() ? $entry->getAfbeeldingBreedte() . 'px' : null,
-            'pageBackground' => $entry->getPaginaAchtergrond(),
-            'direction' => $entry->getLayoutTextRechts() == true ? 'right' : 'left'
-        ]);
-    }
 }
